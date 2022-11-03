17b23a6bd009ce1c6088d3e9e2cd7626429625a4
ttheusLima <ttheus.rlima@gmail.com>
2022-11-03 18:33:03 -0300

    Realizado testes com as classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Iphone");
+
+Iphone iphone = new Iphone("36489M","233Mw45","123456789Im", 512);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\r\n");
+
+Console.WriteLine("Smartphone Nokia");
+
+Nokia nokia = new Nokia("584898aa", "1111", "222Mdas", 8);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
